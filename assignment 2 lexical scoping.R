415f24f4f31e36f06ef22527a7a6e6e97f8530bb
amolkhapli <34803356+amolkhapli@users.noreply.github.com>
2018-01-13 11:13:24 +0530

    Update assignment 2 lexical scoping.R

--- assignment 2 lexical scoping.R
+++ assignment 2 lexical scoping.R
@@ -1,3 +1,10 @@
+##The first function, makeinv creates a special “vector”, 
+##which is really a list containing a function to:
+  
+##set the value of the vector
+##get the value of the vector
+##set the value of the inverse
+##get the value of the inverse
 makeinv <- function(x = numeric()) {
   m <- NULL
   set <- function(y) {
@@ -11,6 +18,12 @@ makeinv <- function(x = numeric()) {
        setinv = setinv,
        getinv = getinv)
 }
+#The following function calculates the matrix inverse of the special “vector” 
+#created with the above function. However, it first checks to see 
+#if the inverse has already been calculated. 
+#If so, it gets the inverse from the cache and skips 
+#the computation. Otherwise, it calculates the inverse of the data 
+#and sets the value of the inverse in the cache via the setinv function.
 cacheinv <- function(x, ...) {
   m <- x$getinv()
   if(!is.null(m)) {
